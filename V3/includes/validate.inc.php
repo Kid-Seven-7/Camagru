7c212cf1e7bb0f3526f4532f487b05b5a3685e8d
Joseph NGOMA <jngoma@e5r7p3.wethinkcode.co.za>
2017-10-20 08:54:31 +0200

    Use only email for login

--- V3/includes/validate.inc.php
+++ V3/includes/validate.inc.php
@@ -3,16 +3,6 @@ if (isset($_POST['sumbit'])) {
   $sql = "SELECT * FROM users WHERE user_email='$email'";
   $result = mysqli_query($conn, $sql);
   $resultcheck = mysqli_num_rows($result);
-  if ($resultcheck > 0) {
-    sql = "SELECT * FROM users WHERE user_id='$uid'";
-    $result = mysqli_query($conn, $sql);
-    $resultcheck = mysqli_num_rows($result);
-    if ($resultcheck > 0) {
-      header("Location: ../signedin.php?signin=invalid_email/password");
-      exit();
-    } else {
-      //CHECK PASSWORD
-    }
   } else {
     //CHECK PASSWORD
   }
